d23c4b24a9b5efd66e9e718ed3e931fb1dcb95f1
samuelyuma <samuelyuma.117@gmail.com>
2025-11-26 08:55:53 +0700

    fix: server and preview port

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -13,8 +12,10 @@ export default defineConfig({
   },
   server: {
     host: true,
+    port: 3000,
   },
   preview: {
     host: true,
+    port 3001,
   },
 });
